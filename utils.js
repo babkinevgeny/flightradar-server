5189c8271c3e5bada9da1b29049fef12a93a8125
babkinevgeny <babkinevgeny@gmail.com>
2021-02-02 20:00:22 +0300

    changed CORS

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 const CORS = {
-  origin: '*',
+  origin: 'https://lbratkovskaya.github.io',
   credentials: true,
+  'Access-Control-Allow-Credentials': true
 };
 
 const PORT = process.env.PORT || 3000;
